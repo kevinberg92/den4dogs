47de0516cf3c21b8546d805479eadeac94d1bc45
Kai Erik Schieren <schierenkai@gmail.com>
2020-06-07 20:10:27 +0200

    logout page added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import EditAccess from "./components/EditAccess/EditAccess";
 import history from "./utils/history";
 import MenuWrapper from "./components/MenuWrapper/MenuWrapper";
 import Loading from "./components/Loading/Loading";
+import LogOut from "./components/LogOut/logout";
 import Error from "./components/Error/Error";
 import { Auth0Context } from "./react-auth0-spa";
 
@@ -23,7 +24,7 @@ import "./App.css";
 
 const App = () => {
   const user = useContext(Auth0Context);
-  const [loggedUser, setLoggedUser] = useState("placeholder");
+  const [loggedUser, setLoggedUser] = useState("kaischieren@hotmail.com");
   const [userIsLoading, setUserIsLoading] = useState(true);
   const [error, setError] = useState(true);
   const { handleDirectCallback, loginWithRedirect, logout } = useAuth0();
@@ -32,7 +33,7 @@ const App = () => {
     return re.test(email);
   } 
     
-
+  
   if (user.isAuthenticated && !user.loading && userIsLoading) {
     console.log("passed" + user)
     if (validateEmail(user.user.email)) {
@@ -62,6 +63,7 @@ const App = () => {
                <Layout>
                  <Switch>
                    <Route exact path="/" render={(props) => <Home {...props} userName={loggedUser} />}/>
+                   <PrivateRoute path="logout" component={LogOut}/>
                    <PrivateRoute path="/dens" render={(props) => <Dens {...props} userName={loggedUser} />}/>
                    <PrivateRoute path="/access" render={(props) => <EditAccess {...props} userName={loggedUser} />}/>
                    <PrivateRoute path="/usage" component={Usage} />
---
+++ src/components/LogOut/logout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class LogOut extends React.Component {
+  render() {
+    return (
+      <div className="chart">
+        LOG OUT
+      </div>
+    );
+  }
+}
+export default LogOut;
